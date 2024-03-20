6d00e00d21959a439350acd9d57481169e86a9ec
Farid Mammadli <fmammadli12101@ada.edu.az>
2024-03-21 01:47:35 +0400

    spring initialized

---
+++ src/test/java/az/edu/ada/library/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
